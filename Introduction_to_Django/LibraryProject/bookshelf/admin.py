7e588192c2cab44bcf27f6c63fc853857faa82a0
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-07-13 10:53:49 +0300

    .

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publication_year')
-    search_filter =('title', 'author')
+    search_fields =('title', 'author')
 
 admin.site.register(Book, BookAdmin)
