a860d8df16799d321cf9eacca561c88c8139af68
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-07-17 20:11:34 +0300

    relationship_app

---
+++ Introduction_to_Django/django-models/relationship_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='book')
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='library')
+class Librarian(models.Model):
+    name = models.CharField()
+    library = models.OneToOneField(Library, on_delete=models.PROTECT, related_name='librarian')
---
+++ Introduction_to_Django/django-models/relationship_app/query_samples.py
@@ -0,0 +1,21 @@
+Python 3.12.3 (main, Jun 18 2025, 17:59:45) [GCC 13.3.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+(InteractiveConsole)
+>>> myauthor = Author.objects.get(name="James Peterson")
+>>> books = myauthor.book.all()
+>>> for book in books:
+...     print(book.title)
+... 
+the fairytale
+the journey
+>>>
+>>> mylibrary = Library.objects.get(name="Kenedy's Library")
+>>> librarybooks = mylibrary.books.all()
+>>> for librarybook in librarybooks:
+...     print(librarybook.title)
+... 
+the journey
+the fairytale
+>>>
+>>> print(mylibrary.librarian.name)
+johnstone pearson
