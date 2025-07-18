193d2094eacba2711b34d936db6109a8d3b5a3bb
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-07-18 11:44:01 +0300

    views

--- django-models/LibraryProject/relationship/urls.py
+++ django-models/LibraryProject/relationship/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from relationship_app import views
+from relationship_app.views import Library_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('books/', views.function_based_view_book, name='books'),
+     path('library/', Library_view.as_view(), name='library'),
+
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Author 
+from .models import Librarian
+from .models import Library
+from .models import Book
+from django.views.generic import DetailView
 
+def function_based_view_book(request):
+    books = Book.objects.all()
+    context ={'book_list': books}
+    return render(request, 'books/book_list.html', context)
 # Create your views here.
+class Library_view(DetailView):
+    model = Library
+    template = 'library/library_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        return context
+
+
