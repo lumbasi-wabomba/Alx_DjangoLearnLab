a432f6cfc5f958d6f279b89b19362d1cd0b5b86e
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-07-18 12:26:29 +0300

    .

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Author 
-from .models import Librarian
-from .models import Library
-from .models import Book
+from .models import Author, Librarian, Library, Book
 from django.views.generic import DetailView
 
 def function_based_view_book(request):
     books = Book.objects.all()
-    context ={'book_list': books}
-    return render(request, 'relationship_app/book_list.html', context)
-# Create your views here.
+    context = {'book_list': books}
+    return render(request, 'relationship_app/list_books.html', context)
+
 class Library_view(DetailView):
     model = Library
-    template = 'library/library_list.html'
+    template_name = 'relationship_app/library_detail.html'  
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         library = self.get_object()
+        context['library'] = library 
         return context
-
-
