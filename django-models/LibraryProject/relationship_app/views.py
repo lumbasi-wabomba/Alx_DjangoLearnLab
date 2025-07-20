f77e402fe2730f48146b647c90913d4b80b1dd56
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-07-20 20:36:58 +0300

    ...

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .models import Author, Librarian, Library, Book
 from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 # View to list all books
 def function_based_view_book(request):
@@ -27,10 +29,13 @@ def profile(request):
     return render(request, 'relationship_app/profile.html')
 
 def logout(request):
+    logout(request)
     return render(request, 'relationship_app/logout.html')
 
 def login(request):
+    login(request)
     return render(request, 'relationship_app/login.html')
 
 def register(request):
+    UserCreationForm(request)
     return render(request, 'relationship_app/register.html')
