5383a7615bf3c1075af38208b7f974d05b9fd2ca
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-07-20 19:30:26 +0300

    ..

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from .views import list_books, LibraryDetailView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', views.function_based_view_book, name='books'),
-    path('library/', Library_view.as_view(), name='library'),
+    path('library/', LibraryDetailview.as_view(), name='library'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
