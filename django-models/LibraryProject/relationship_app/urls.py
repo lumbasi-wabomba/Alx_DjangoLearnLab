7acdec76935dd7898c578cf996ab2503111ccfc0
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-07-20 21:10:56 +0300

    ...

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from relationship_app import views
 from relationship_app.views import LibraryDetailview  
+from .views import LoginView, LogoutView, RegisterView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('books/', views.function_based_view_book, name='books'),
-    path('library/<int:pk>/', LibraryDetailview.as_view(), name='library'),  # Detail view
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('register/', views.register, name='register'),
+    path('library/<int:pk>/', LibraryDetailview.as_view(), name='library'),  
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', views.profile, name='profile')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Author, Librarian, Library, Book
-from django.views.generic.detail import DetailView
+from django.views.generic import DetailView, FormView , RedirectView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.urls import reverse_lazy
+
 
 # View to list all books
 def function_based_view_book(request):
@@ -23,19 +25,36 @@ class LibraryDetailview(DetailView):
         context['library'] = library 
         return context
 
+class LoginView(FormView):
+    template_name = 'relationship_app/login.html'
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        user = form.get_user()
+        auth_login(self.request, user)
+        return super().form_valid(form)
+
+class LogoutView(RedirectView):
+    pattern_name = 'login'
+
+    def get(self, request, *args, **kwargs):
+        auth_logout(request)
+        return super().get(request, *args, **kwargs)
+
+class RegisterView(FormView):
+    template_name = 'relationship_app/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return super().form_valid(form)
+
+
+
 # Auth views
 @login_required
 def profile(request):
     return render(request, 'relationship_app/profile.html')
-
-def logout(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
-
-def login(request):
-    login(request)
-    return render(request, 'relationship_app/login.html')
-
-def register(request):
-    UserCreationForm(request)
-    return render(request, 'relationship_app/register.html')
