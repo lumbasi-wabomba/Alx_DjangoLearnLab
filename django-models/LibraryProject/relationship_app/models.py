04cbb6c903916a4892713a2222f07e473c4d4669
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-07-20 21:39:57 +0300

    ...

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -23,3 +24,17 @@ class Librarian(models.Model):
     library = models.OneToOneField(Library, on_delete=models.PROTECT, related_name='librarian')
     def __str__(self):
         return self.name
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,9 @@ from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from .models import Book, Library
 
+from django.contrib.auth.decorators import user_passes_test, login_required
+from .models import UserProfile
+
 def function_based_view_book(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'book_list': books})
@@ -43,3 +46,31 @@ class RegisterView(FormView):
 @login_required
 def profile(request):
     return render(request, 'relationship_app/profile.html')
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test, login_required
+from .models import UserProfile
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+@login_required
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+@login_required
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+@login_required
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
