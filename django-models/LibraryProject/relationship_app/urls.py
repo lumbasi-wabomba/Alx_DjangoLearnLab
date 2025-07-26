0e9e34b1f254ede8655dbb40217ec849b108ad76
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-07-26 14:03:13 +0300

    added role based activities

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
-    path('books/add/', add_book_view, name='add_book'),
-    path('books/<int:pk>/edit/', edit_book_view, name='edit_book'),
-    path('books/<int:pk>/delete/', delete_book_view, name='delete_book'),
+    path('books/add_book/', add_book_view, name='add_book'),
+    path('books/<int:pk>/edit_book/', edit_book_view, name='edit_book'),
+    path('books/<int:pk>/delete_book/', delete_book_view, name='delete_book'),
 ]
