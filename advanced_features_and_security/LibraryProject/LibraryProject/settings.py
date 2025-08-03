3a96b58cffab4175a78405e94e4c908a605e06d9
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-08-03 23:53:18 +0300

    check1

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'LibraryProject.wsgi.application'
+# settings.py
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 
 # Database
