753a00b408312247fe86e175d53d06a2472b44e1
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-08-08 15:21:12 +0300

    changes in custom drf

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,8 +2,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 # Create your views here.
 
 class ListView(generics.ListAPIView):
