88e0184d8882bd1be1cca8e182a0a36cb02b6eb4
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-08-08 12:14:44 +0300

    started a new project that includes custom drf

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Book
+from .models import Author
+from datetime import date
+
+
+class BookSerializer(serializers.ModelSerializer):
+    model = Book
+    fields = '__all__'
+
+class AuthorSerializer(serializers.ModelSerializer):
+    model = Author
+    fields = ['name']
+    book = BookSerializer(many=True, read_only=True)
+
+    
+    def validate_publication_year(self, data):
+        if date().year(data['publication_year']) > date.today().year(): 
+            raise serializers.ValidationError("the date is in the future")
+        return data
