51f0101eccf3c112fe02701f98ba6359b8364857
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-08-10 08:46:17 +0300

    added filters to the drf

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,11 +3,15 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
 # Create your views here.
 
 class ListView(generics.ListAPIView):
     authentication_classes =[TokenAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends =[filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'author', 'publication_year']
+    ordering_fields = ['title', 'publication_year']
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
