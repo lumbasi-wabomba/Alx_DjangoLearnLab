6e8c7c7b7f3c8fee0d699411f1319468e0968188
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-08-29 19:57:43 +0300

    ...

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('', include('posts.urls')),
+    path('api/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
 ]
