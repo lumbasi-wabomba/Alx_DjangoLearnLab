aafe06e7e3b6663eb0233d692a181cc80df62f06
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-08-29 22:33:03 +0300

    ...

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from rest_framework.generics import CreateAPIView
 from rest_framework import generics
 from rest_framework.views import APIView
+from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import AllowAny
 from rest_framework.authentication import TokenAuthentication
