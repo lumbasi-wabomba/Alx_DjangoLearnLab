96fcb76b53427bb840fcf45f3cf1e179ca94f2f6
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-08-29 19:56:17 +0300

    ...

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
